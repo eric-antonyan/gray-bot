61658576f360b267331db50551e58521b19080d5
gray <mrgrayofficial@protonmail.com>
2024-10-25 12:45:16 +0400

    go to bot button

--- main.py
+++ main.py
@@ -198,7 +198,7 @@ async def webapp_command(message: types.Message):
     button = InlineKeyboardButton(text="Բացել խաղը", web_app=web_app)
     keyboard = InlineKeyboardMarkup(inline_keyboard=[[button]])
 
-    bot_link_button = InlineKeyboardButton(text="Բացել bot-ը", url="https://t.me/@GrayQuizz_bot")
+    bot_link_button = InlineKeyboardButton(text="Բացել bot-ը", url="@GrayQuizz_bot")
     bot_link_keyboard = InlineKeyboardMarkup(inline_keyboard=[[bot_link_button]])
 
     try:
